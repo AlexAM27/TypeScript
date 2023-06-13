9964e645aa417f1798247d8dfca2f03ea1316c02
Alexander Markin <alexander.markin@creamteam.de>
2023-06-13 16:49:46 +0200

    decorators

--- dragAndDrop/src/app.ts
+++ dragAndDrop/src/app.ts
@@ -1,8 +1,25 @@
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
 class ProjectInput {
 
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+  
   
   constructor() {
     this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
@@ -10,7 +27,45 @@ class ProjectInput {
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+    
+    this.configure();
+
     this.attach();
+  }
+
+  @autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    const userInput = this.gatherUserInput()
+    if(userInput) {
+      console.log(userInput)
+      this.clearInput();
+    }
+  }
+
+  private clearInput() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
+  }
+
+  private gatherUserInput(): [string, string, number] | undefined {
+    const title = this.titleInputElement.value;
+    const description = this.descriptionInputElement.value;
+    const peopleAmount = this.peopleInputElement.value;
+    if (title.length === 0 || description.length === 0 || peopleAmount.length === 0) {
+      alert('Wrong input!');
+      return;
+    } else {
+      return [title, description, +peopleAmount];
+    }    
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler)
   }
 
   private attach() {
