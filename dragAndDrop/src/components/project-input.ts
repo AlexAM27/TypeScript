d6d821b68e9fc3430099c0c644b8af4acb9ca3d8
Alexander Markin <alexander.markin@creamteam.de>
2023-06-22 20:12:17 +0200

    namespaces

---
+++ dragAndDrop/src/components/project-input.ts
@@ -0,0 +1,83 @@
+/// <reference path="base-component.ts"/>
+/// <reference path="../util/validation.ts"/>
+/// <reference path="../decorators/autobind.ts"/>
+/// <reference path="../state/project-state.ts"/>
+
+
+namespace App {
+  export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>{
+
+    titleInputElement: HTMLInputElement;
+    descriptionInputElement: HTMLInputElement;
+    peopleInputElement: HTMLInputElement;
+    
+    constructor() {
+      super('project-input', 'app', true)
+  
+      this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+      
+      this.configure();
+  
+    }
+  
+    configure() {
+      this.element.addEventListener('submit', this.submitHandler)
+    }
+  
+    renderContent(): void {
+        
+    }
+  
+    @autobind
+    private submitHandler(event: Event) {
+      event.preventDefault();
+      const userInput = this.gatherUserInput()
+      if(Array.isArray(userInput)) {
+        const [title, description, people] = userInput;
+        projectState.addProject(title, description, people);
+        this.clearInput();
+      }
+    }
+  
+    private clearInput() {
+      this.titleInputElement.value = '';
+      this.descriptionInputElement.value = '';
+      this.peopleInputElement.value = '';
+    }
+  
+    private gatherUserInput(): [string, string, number] | undefined {
+      const title = this.titleInputElement.value;
+      const description = this.descriptionInputElement.value;
+      const peopleAmount = this.peopleInputElement.value;
+  
+      const titleValidatable: Validatable = {
+        value: title,
+        required: true
+      }; 
+  
+      const descriptionValidatable: Validatable = {
+        value: description,
+        required: true,
+        minLength: 5,
+        maxLength: 10
+      }; 
+  
+      const peopleValidatable: Validatable = {
+        value: peopleAmount,
+        required: true,
+        min: 1,
+        max: 5
+      }; 
+  
+      if (!validate(titleValidatable) || !validate(descriptionValidatable) || !validate(peopleValidatable)) {
+        alert('Wrong input!');
+        return;
+      } else {
+        return [title, description, +peopleAmount];
+      }    
+    }
+  
+  }
+}
